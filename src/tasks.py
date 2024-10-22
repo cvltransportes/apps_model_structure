baaab25cdde08e08f01427328e944f471ac888bc
Carvalima Transportes <114350154+cvltransportes@users.noreply.github.com>
2024-10-22 11:11:19 -0400

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -1,3 +1,7 @@
+import sys ,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from settings import *
 import src.dataprocessing as dp
 from priority_classes.datahandler.datahandler import Handler
 
