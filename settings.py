055f6335fd260a25f07a766dd44f2c1fc3486f30
Carvalima Transportes <114350154+cvltransportes@users.noreply.github.com>
2024-10-22 11:09:36 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+import logging as log
 
 EMAIL_DEBUG = 'qualidade.10@carvalima.com.br'
 
@@ -6,3 +7,14 @@ BASE_DIR = os.path.dirname(__file__)
 
 PATH_DOWNLOADS = os.path.join(BASE_DIR,'downloads')
 PATH_RELATORIOS = os.path.join(BASE_DIR,'relatorios')
+
+# logging
+LOG_FILENAME = None
+
+log.basicConfig(
+    level=log.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename= LOG_FILENAME
+)
+logging = log.getLogger(__name__)
