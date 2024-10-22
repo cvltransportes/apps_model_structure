6c9cbb82605e24450ad357e42483b3bec34ce459
Carvalima Transportes <114350154+cvltransportes@users.noreply.github.com>
2024-10-22 11:10:52 -0400

    Update dataprocessing.py

--- src/dataprocessing.py
+++ src/dataprocessing.py
@@ -1,3 +1,7 @@
+import sys ,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from settings import *
 from priority_classes.datahandler.datahandler import Handler
 
 hd = Handler()
